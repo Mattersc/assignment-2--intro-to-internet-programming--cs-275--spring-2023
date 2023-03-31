af489bbcdadcc746b4a6661ac972f6f5936bb686
Carter Matters <matters@hartford.edu>
2023-03-30 20:29:26 -0400

    Update main.js
    added initial javascript for json data

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
-window.onload = () => {
-       alert('Test');                    
+let carouselSlides = document.getElementsByClassName(`carousel-slides`);
+
+function albumData(data) {
+
+    for (let i = 0; i < data.slide.length; i++) {
+        let artist = data.slide[i].artist;
+        let albumName = data.slide[i].album;
+        let coverImage = data.slide[i].cover_image;
+        let review = data.slide[i].review;
+        console.log(artist, albumName, coverImage, review);
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let script = document.createElement(`script`);
+script.src = `json/data.json`;
+document.body.appendChild(script);
